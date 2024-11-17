482a066befb8c580b5f856036b12736b430804c3
Indrajeet Patil <patilindrajeet.science@gmail.com>
2024-11-17 11:37:51 +0100

    revert styling change

--- media/outlier_example.R
+++ media/outlier_example.R
@@ -1,4 +1,3 @@
-
 library(ggstatsplot)
 library(datasauRus)
 here::here()
@@ -6,11 +5,13 @@ here::here()
 set.seed(123)
 df <- dplyr::filter(datasaurus_dozen, dataset == "dino")
 
-
-ggplot2::ggsave(ggscatterstats(df, x, y, type = "p",
-                               ggtheme = hrbrthemes::theme_ipsum_tw(),
-                               title = "Relationship between x and y"),
-  filename = paste0(here::here(), "/docs/slides/images/after_ggstats.PNG"),
+ggplot2::ggsave(
+  ggscatterstats(df, x, y,
+    type = "p",
+    ggtheme = hrbrthemes::theme_ipsum_tw(),
+    title = "Relationship between x and y"
+  ),
+  filename = file.path(here::here(), "media", "after_ggstats.PNG"),
   height = 5,
   width = 6,
   units = "in"
